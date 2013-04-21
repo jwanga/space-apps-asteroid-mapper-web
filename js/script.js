5c40735b6d926f775904355014393646cd60ae50
Yeison Rodriguez <dev.yeison@gmail.com>
2013-04-21 16:50:38 -0400

    A little bit of keypress oragnizing

--- js/script.js
+++ js/script.js
@@ -1,5 +1,65 @@
 jQuery(document).ready(function($){
 
+        ////////////////////////////
+        // Vertical slider
+        function createSlider(){
+            $("#v-slider").slider({
+                orientation: "vertical",
+                range: "min",
+                min: 0,
+                max: 100,
+                value: 50,
+                slide: function (event, ui) {
+                    $("#amount").val(ui.value);
+                }
+            });
+            $("#amount").val($("#v-slider").slider("value"));
+        }
+
+        function sliderValue(e){
+
+            var ele = $(e);
+            var ele_sub = ele[0];
+            ele_sub.setAttribute('data-magnitude', $("#v-slider").slider("value"));
+
+        }
+
+        createSlider();
+
+	var socket = io.connect('http://localhost:8080');	
+
+	$('.btn').each(function(){
+
+		$(this).click(function(){
+
+                        sliderValue(this);
+
+			var move = $(this).attr('data-move');
+                        var magnitude = $(this).attr('data-magnitude');
+
+                        socket.emit('moving', { my: move , magnitude: magnitude });
+
+                        console.log( move + ' by ' + magnitude);
+
+		})
+	})
+
+	// io.sockets.on('connection', function (socket) {
+
+		socket.on('moving-back', function(data){
+
+			var height = $('#console').get(0).scrollHeight; 
+
+			console.log( );
+
+			$('#console').append( "<p>" + data + "</p>").scrollTop( height);
+
+
+		})
+
+	// })
+
+
         my_combos = [
             {
                 "keys"          : "q",
@@ -64,66 +124,5 @@ jQuery(document).ready(function($){
         ];
 
         keypress.register_many(my_combos);
-
-
-        ////////////////////////////
-        // Vertical slider
-        function createSlider(){
-            $("#v-slider").slider({
-                orientation: "vertical",
-                range: "min",
-                min: 0,
-                max: 100,
-                value: 50,
-                slide: function (event, ui) {
-                    $("#amount").val(ui.value);
-                }
-            });
-            $("#amount").val($("#v-slider").slider("value"));
-        }
-
-        function sliderValue(e){
-
-            var ele = $(e);
-            var ele_sub = ele[0];
-            ele_sub.setAttribute('data-magnitude', $("#v-slider").slider("value"));
-
-        }
-
-        createSlider();
-
-	var socket = io.connect('http://localhost:8080');	
-
-	$('.btn').each(function(){
-
-		$(this).click(function(){
-
-                        sliderValue(this);
-
-			var move = $(this).attr('data-move');
-                        var magnitude = $(this).attr('data-magnitude');
-
-                        socket.emit('moving', { my: move , magnitude: magnitude });
-
-                        console.log( move + ' by ' + magnitude);
-
-		})
-	})
-
-	// io.sockets.on('connection', function (socket) {
-
-		socket.on('moving-back', function(data){
-
-			var height = $('#console').get(0).scrollHeight; 
-
-			console.log( );
-
-			$('#console').append( "<p>" + data + "</p>").scrollTop( height);
-
-
-		})
-
-	// })
-
 
 })
