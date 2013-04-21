8bf5abe1b354846a54f9b37d4f42338743cadbd6
Sergey Kishchenko <voidwrk@gmail.com>
2013-04-21 09:49:47 +0300

    video is now working

--- js/app.js
+++ js/app.js
@@ -3,25 +3,46 @@ var app = require('http').createServer(handler)
   , fs = require('fs')
 
 app.listen(8080); // I changed the port from 80 to 8080 incase you are running a web server
+var png = null;
+var client = null;
 
 function handler (req, res) {
-  fs.readFile(__dirname + '/index.html',
-  function (err, data) {
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error loading index.html');
-    }
+  // fs.readFile(__dirname + '/index.html',
+  // function (err, data) {
+  //   if (err) {
+  //     res.writeHead(500);
+  //     return res.end('Error loading index.html');
+  //   }
 
-    res.writeHead(200);
-    res.end(data);
-  });
+  //   res.writeHead(200);
+  //   res.end(data);
+  // });
+  if (!client)
+  {
+    res.writeHead(404);
+    return;
+  }
+  if (!png) {
+      png = client.createPngStream({ log : process.stderr });
+      png.on('error', function (err) {
+          console.error('png stream ERROR: ' + err);
+      });
+    }
+  res.writeHead(200, { 'Content-Type': 'multipart/x-mixed-replace; boundary=--daboundary' });
+  png.on('data', sendPng);
+
+  function sendPng(buffer) {
+    console.log(buffer.length);
+    res.write('--daboundary\nContent-Type: image/png\nContent-length: ' + buffer.length + '\n\n');
+    res.write(buffer);
+  }
 }
+
 
 io.sockets.on('connection', function (socket) {
   
   socket.emit('news', { hello: 'world' });
   
-  // require('dronestream').listen(app);
 
 
   socket.on('my other event', function (data) {
@@ -29,14 +50,10 @@ io.sockets.on('connection', function (socket) {
   });
 
 
-    var arDrone = require('ar-drone');
-    var client  = arDrone.createClient();
+  var arDrone = require('ar-drone');
+  client = arDrone.createClient();
 
   socket.on('moving' , function(data){
-
-
-
-
     // client.config('general:navdata_demo', 'FALSE');
 
     if( data.my == 'take-off' ){
